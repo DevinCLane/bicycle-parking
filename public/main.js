b41fbd1079f7bb91f31a70764cd375c0a802743b
Devin Lane <devin.c.lane@gmail.com>
2023-03-01 12:26:18 -0800

    initial commit

---
+++ public/main.js
@@ -0,0 +1,68 @@
+mapboxgl.accessToken = process.env.API_KEY
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v11',
+    zoom: 12,
+    center: [-122.44442, 37.75493]
+})
+
+map.on('load', () => {
+    map.addSource('bicycle-parking', {
+        'type': 'geojson',
+        'data': 'https://data.sfgov.org/resource/hn4j-6fx5.geojson'
+    })
+    map.addLayer({
+        'id': 'bicycle-parking-layer',
+        'source': 'bicycle-parking',
+        'type': 'circle',
+        'paint': {
+            'circle-radius': 4,
+            'circle-stroke-width': 2,
+            'circle-color': 'red',
+            'circle-stroke-color': 'white'
+        }
+    })
+
+    const popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    })
+
+    map.on('mouseenter', 'bicycle-parking-layer', (e) => {
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const address = e.features[0].properties.address;
+        const location = e.features[0].properties.location;
+        const placement = e.features[0].properties.placement;
+        const racks = e.features[0].properties.racks;
+        const spaces = e.features[0].properties.spaces;
+        const installYear = e.features[0].properties.install_yr;
+        popup
+            .setLngLat(coordinates)
+            .setHTML(
+                "<h3>" + address + "</h3>"
+                + "<table>" 
+                    + "<tbody>" 
+                    + "<tr><th>Location</th><td>" + location + "</td></tr>" 
+                    + "<tr><th>Placement</th><td>" + placement + "</td></tr>" 
+                    + "<tr><th>Racks</th><td>" + racks + "</td></tr>" 
+                    + "<tr><th>Spaces</th><td>" + spaces + "</td></tr>" 
+                    + "<tr><th>Install Year</th><td>" + installYear + "</td></tr>"
+                    + "</tbody>"
+                + "</table>"
+            )
+            .addTo(map)
+    })
+
+    // change the cursor to a pointer when we hover over a bicycle rack
+    map.on('mouseenter', 'bicycle-parking-layer', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when we mouse away
+    map.on('mouseleave', 'bicycle-parking-layer', () => {
+        map.getCanvas().style.cursor = '';
+        popup.remove()
+    });
+})
+
