3eff983bded5681946393490b5c739462e83c8bd
Chris Antaki <ChrisAntaki@gmail.com>
2023-03-03 19:20:56 -0800

    Formatting with Prettier

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ https://user-images.githubusercontent.com/8145785/222561584-8521e840-2536-4cf2-b
 
 [See the live project here](https://bicycleparking.netlify.app/)
 
-A map that shows available bicycle parking in San Francisco. 
+A map that shows available bicycle parking in San Francisco.
 
 ## How it's made
 
@@ -14,12 +14,13 @@ A map that shows available bicycle parking in San Francisco.
 
 - These data points are added to the map as circles
 
-- When the user hovers over a data point, a pop up is shown with the relevant data about the bicycle parking location: 
-    - location
-    - placement
-    - number of racks 
-    - number of spaces
-    - install year.
+- When the user hovers over a data point, a pop up is shown with the relevant data about the bicycle parking location:
+
+  - location
+  - placement
+  - number of racks
+  - number of spaces
+  - install year.
 
 - We also change the cursor to a pointer when hovering over a data point.
 
@@ -27,7 +28,7 @@ A map that shows available bicycle parking in San Francisco.
 
 - Mapbox allows you to create access tokens on a per-project basis, and restrict access to those tokens to specified URLs. I was building out an API proxy server to hold my API key safely, and then learned that I could simply keep the access token in my frontend code, but restrict the URL access to the URL of the project. Therefore, no one can use this access token unless the request is coming from the URL of this same project.
 
-- Mapbox programming centers around defining a Map object, to which we pass the container, a Mapbox style, Zoom amount, and the center of the latitude/longitude. 
+- Mapbox programming centers around defining a Map object, to which we pass the container, a Mapbox style, Zoom amount, and the center of the latitude/longitude.
 
 - Importantly, Mapbox GL uses longitude, latitude coordinate order (as opposed to latitude, longitude) to match GeoJSON. Putting these values in reverse will result in the map not loading.
 
--- main.js
+++ main.js
@@ -1,75 +1,88 @@
 // Don't worry, this access token is restricted to only allow usage from the domain that it's deployed on.
 // It cannot be taken and used to run up my account 😅
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGV2aW5sYW5lIiwiYSI6ImNsZXJrdDlwcjBmNXY0NW5sNW44aTQzNW8ifQ.nCItJdtqehnnUcXOrtcupQ'
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZGV2aW5sYW5lIiwiYSI6ImNsZXJrdDlwcjBmNXY0NW5sNW44aTQzNW8ifQ.nCItJdtqehnnUcXOrtcupQ";
 
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v11',
-    zoom: 12,
-    center: [-122.44442, 37.75493],
-    customAttribution: 'Map by <a href="https://www.devinlane.com/" target="_blank">Devin Lane 🎸</a> <a href="https://github.com/DevinCLane/bicycle-parking" target="_blank">Source code here 💻</a>'
-})
+  container: "map",
+  style: "mapbox://styles/mapbox/light-v11",
+  zoom: 12,
+  center: [-122.44442, 37.75493],
+  customAttribution:
+    'Map by <a href="https://www.devinlane.com/" target="_blank">Devin Lane 🎸</a> <a href="https://github.com/DevinCLane/bicycle-parking" target="_blank">Source code here 💻</a>',
+});
 
-map.on('load', () => {
-    map.addSource('bicycle-parking', {
-        'type': 'geojson',
-        'data': 'https://data.sfgov.org/resource/hn4j-6fx5.geojson'
-    })
-    map.addLayer({
-        'id': 'bicycle-parking-layer',
-        'source': 'bicycle-parking',
-        'type': 'circle',
-        'paint': {
-            'circle-radius': 4,
-            'circle-stroke-width': 2,
-            'circle-color': 'red',
-            'circle-stroke-color': 'white'
-        }
-    })
+map.on("load", () => {
+  map.addSource("bicycle-parking", {
+    type: "geojson",
+    data: "https://data.sfgov.org/resource/hn4j-6fx5.geojson",
+  });
+  map.addLayer({
+    id: "bicycle-parking-layer",
+    source: "bicycle-parking",
+    type: "circle",
+    paint: {
+      "circle-radius": 4,
+      "circle-stroke-width": 2,
+      "circle-color": "red",
+      "circle-stroke-color": "white",
+    },
+  });
 
-    const popup = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false
-    })
+  const popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false,
+  });
 
-    map.on('mouseenter', 'bicycle-parking-layer', (e) => {
-        const coordinates = e.features[0].geometry.coordinates.slice();
+  map.on("mouseenter", "bicycle-parking-layer", (e) => {
+    const coordinates = e.features[0].geometry.coordinates.slice();
 
-        const {
-            address,
-            install_yr: installYear,
-            location,
-            placement,
-            racks,
-            spaces
-        } = e.features[0].properties;
+    const {
+      address,
+      install_yr: installYear,
+      location,
+      placement,
+      racks,
+      spaces,
+    } = e.features[0].properties;
 
-        popup
-            .setLngLat(coordinates)
-            .setHTML(
-                "<h3>" + address + "</h3>"
-                + "<table>"
-                + "<tbody>"
-                + "<tr><th>Location</th><td>" + location + "</td></tr>"
-                + "<tr><th>Placement</th><td>" + placement + "</td></tr>"
-                + "<tr><th>Racks</th><td>" + racks + "</td></tr>"
-                + "<tr><th>Spaces</th><td>" + spaces + "</td></tr>"
-                + "<tr><th>Install Year</th><td>" + installYear + "</td></tr>"
-                + "</tbody>"
-                + "</table>"
-            )
-            .addTo(map)
-    })
+    popup
+      .setLngLat(coordinates)
+      .setHTML(
+        "<h3>" +
+          address +
+          "</h3>" +
+          "<table>" +
+          "<tbody>" +
+          "<tr><th>Location</th><td>" +
+          location +
+          "</td></tr>" +
+          "<tr><th>Placement</th><td>" +
+          placement +
+          "</td></tr>" +
+          "<tr><th>Racks</th><td>" +
+          racks +
+          "</td></tr>" +
+          "<tr><th>Spaces</th><td>" +
+          spaces +
+          "</td></tr>" +
+          "<tr><th>Install Year</th><td>" +
+          installYear +
+          "</td></tr>" +
+          "</tbody>" +
+          "</table>"
+      )
+      .addTo(map);
+  });
 
-    // change the cursor to a pointer when we hover over a bicycle rack
-    map.on('mouseenter', 'bicycle-parking-layer', () => {
-        map.getCanvas().style.cursor = 'pointer';
-    });
+  // change the cursor to a pointer when we hover over a bicycle rack
+  map.on("mouseenter", "bicycle-parking-layer", () => {
+    map.getCanvas().style.cursor = "pointer";
+  });
 
-    // Change it back to a pointer when we mouse away.
-    map.on('mouseleave', 'bicycle-parking-layer', () => {
-        map.getCanvas().style.cursor = '';
-        popup.remove()
-    });
-})
-
+  // Change it back to a pointer when we mouse away.
+  map.on("mouseleave", "bicycle-parking-layer", () => {
+    map.getCanvas().style.cursor = "";
+    popup.remove();
+  });
+});
